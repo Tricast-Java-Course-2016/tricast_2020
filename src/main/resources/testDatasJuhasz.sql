3c6b6df2579fe0b133a40636764a806949a0c28d
Juhász Kristóf <juhkris96@gmail.com>
2020-03-10 21:19:29 +0100

    WHT-12 test datas

--- src/main/resources/testDatasJuhasz.sql
+++ src/main/resources/testDatasJuhasz.sql
@@ -2,9 +2,11 @@
 INSERT INTO TIMEOFFCAL.roles(id, roleName) VALUES(1, 'admin');
 INSERT INTO TIMEOFFCAL.roles(id, roleName) VALUES(2, 'user');
 
+INSERT INTO TIMEOFFCAL.rolePermissionSet(id, roleId, permissionId) VALUES (1, 1, 1);
+INSERT INTO TIMEOFFCAL.rolePermissionSet(id, roleId, permissionId) VALUES (2, 2, 2);
 
-INSERT INTO TIMEOFFCAL.permissions(id, permissionName) VALUES(1, 'everything');
-INSERT INTO TIMEOFFCAL.permissions(id, permissionName) VALUES(2, 'not everything');
+INSERT INTO TIMEOFFCAL.permissions(id, permissionName) VALUES(1, 'Accept Holiday');
+INSERT INTO TIMEOFFCAL.permissions(id, permissionName) VALUES(2, 'View Dashboard');
 
 
 
@@ -27,14 +29,14 @@ INSERT INTO TIMEOFFCAL.workDays(id, userId, date) VALUES (4, 2, '2020-05-10');
 
 
 INSERT INTO TIMEOFFCAL.worktimes(id, startTime, endTime, "type", "comment", modifiedBy, workDayId)
-								VALUES(1, '2020-04-10 8:00', '2020-04-09 17:00', 'normal', 'Stinky Day', 1, 1);
+								VALUES(1, '2020-04-09 8:00', '2020-04-09 17:00', 'normal', 'Normal Day', 1, 1);
 INSERT INTO TIMEOFFCAL.worktimes(id, startTime, endTime, modifiedStartTime, modifiedEndTime, "type", "comment", modifiedBy, workDayId)
-								VALUES(2, '2020-04-10 8:00', '2020-04-09 17:00', '2020-04-10 9:00', '2020-04-10 18:00', 'normal', 'Stinky Day', 1, 2);
+								VALUES(2, '2020-04-09 8:00', '2020-04-09 17:00', '2020-04-10 9:00', '2020-04-10 18:00', 'normal', 'Happy Day', 1, 2);
 
 INSERT INTO TIMEOFFCAL.worktimes(id, startTime, endTime, "type", "comment", modifiedBy, workDayId)
-								VALUES(3, '2020-04-10 8:00', '2020-04-09 17:00', 'normal', 'Stinky Day', 1, 3);
+								VALUES(3, '2020-05-09 8:00', '2020-05-09 17:00', 'normal', 'Normal Day', 1, 3);
 INSERT INTO TIMEOFFCAL.worktimes(id, startTime, endTime, modifiedStartTime, modifiedEndTime, "type", "comment", modifiedBy, workDayId)
-								VALUES(4, '2020-04-10 8:00', '2020-04-09 17:00', '2020-04-10 9:00', '2020-04-10 18:00', 'normal', 'Stinky Day', 1, 4);
+								VALUES(4, '2020-05-09 8:00', '2020-05-09 17:00', '2020-04-10 9:00', '2020-04-10 18:00', 'normal', 'Happy Day', 1, 4);
 
 
 
@@ -43,12 +45,12 @@ INSERT INTO TIMEOFFCAL.worktimes(id, startTime, endTime, modifiedStartTime, modi
 INSERT INTO TIMEOFFCAL.offDays(id, "date", "type", status, approvedBy, userId, startTime, endTime)
 								Values(1, '2020-04-13', 'freeday', 'accepted', 1, 1, '2020-04-13 8:00', '2020-04-13 12:00');
 INSERT INTO TIMEOFFCAL.offDays(id, "date", "type", status, approvedBy, userId, startTime, endTime)
-								Values(2, '2020-04-14', 'freeday', 'accepted', 1, 1, '2020-04-14 8:00', '2020-04-22 00:00');
+								Values(2, '2020-04-14', 'freeday', 'accepted', 1, 1, '2020-04-14 8:00', '2020-04-14 17:00');
 
 INSERT INTO TIMEOFFCAL.offDays(id, "date", "type", status, approvedBy, userId, startTime, endTime)
-								Values(3, '2020-05-13', 'freeday', 'accepted', 1, 2, '2020-04-13 8:00', '2020-04-13 12:00');
+								Values(3, '2020-05-13', 'freeday', 'accepted', 1, 2, '2020-05-13 8:00', '2020-05-13 12:00');
 INSERT INTO TIMEOFFCAL.offDays(id, "date", "type", status, approvedBy, userId, startTime, endTime)
-								Values(4, '2020-06-14', 'freeday', 'accepted', 1, 2, '2020-04-14 8:00', '2020-04-22 00:00');
+								Values(4, '2020-05-14', 'freeday', 'accepted', 1, 2, '2020-05-14 8:00', '2020-05-14 17:00');
 
 
 
