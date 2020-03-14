dceb08a4c31879b01d8350697f134fb2eeae4ebd
Albrecht Márk <alb.mark94@gmail.com>
2020-03-14 12:09:06 +0100

    WHT-21 Created specialday's repository

---
+++ src/main/java/com/tricast/repositories/SpecialdayRepository.java
@@ -0,0 +1,9 @@
+package com.tricast.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tricast.repositories.entities.Specialday;
+
+public interface SpecialdayRepository extends CrudRepository<Specialday, Long>{
+
+}
