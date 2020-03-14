e0ef01c28f93c931a34e1848b67a0f200b1138bd
Nagy István <mrtireal@gmail.comn>
2020-03-14 17:28:50 +0100

    WHT-18 Created OffdayLimits repository

---
+++ src/main/java/com/tricast/repositories/OffdayLimitsRepository.java
@@ -0,0 +1,9 @@
+package com.tricast.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tricast.repositories.entities.OffdayLimits;
+
+public interface OffdayLimitsRepository extends CrudRepository<OffdayLimits, Long> {
+
+}
