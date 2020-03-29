b496af38e87fea1ef4c03b516602c07aefaaafb6
Albrecht Márk <alb.mark94@gmail.com>
2020-03-29 17:30:03 +0200

    Added getAllByDateBetween method

--- src/main/java/com/tricast/repositories/SpecialdayRepository.java
+++ src/main/java/com/tricast/repositories/SpecialdayRepository.java
@@ -1,9 +1,13 @@
 package com.tricast.repositories;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tricast.repositories.entities.Specialday;
 
 public interface SpecialdayRepository extends CrudRepository<Specialday, Long>{
-
+	
+	List<Specialday> getAllByDateBetween(ZonedDateTime startDateTime , ZonedDateTime finishDate);
 }
