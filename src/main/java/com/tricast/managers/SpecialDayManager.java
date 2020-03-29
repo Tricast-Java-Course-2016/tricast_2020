dc4a334c9763b515050d831fba2f10c333709058
Albrecht Márk <alb.mark94@gmail.com>
2020-03-29 17:27:45 +0200

    Created specialday manager with create, delete and get method

---
+++ src/main/java/com/tricast/managers/SpecialDayManager.java
@@ -0,0 +1,13 @@
+package com.tricast.managers;
+
+import java.util.List;
+
+import com.tricast.repositories.entities.Specialday;
+
+public interface SpecialDayManager {
+	List<Specialday> getSpecialDaysInTheYear(String year);
+	
+	Specialday createSpecialday(Specialday specialdayRequest);
+	
+	void deleteSpecialday(int id);
+}
