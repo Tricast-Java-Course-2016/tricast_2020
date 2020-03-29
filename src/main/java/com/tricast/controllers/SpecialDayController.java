db04a463b7ca6551f48304fd7457bac2f6f4387e
Albrecht Márk <alb.mark94@gmail.com>
2020-03-29 17:26:30 +0200

    Created specialDays controller with gets, save, delete methods

---
+++ src/main/java/com/tricast/controllers/SpecialDayController.java
@@ -0,0 +1,45 @@
+package com.tricast.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tricast.managers.SpecialDayManager;
+import com.tricast.repositories.entities.Specialday;
+
+@RestController
+@RequestMapping(path = "rest/specialdays")
+public class SpecialDayController {
+	
+
+	@Autowired
+	private SpecialDayManager specialDayManager;
+	
+	@GetMapping
+	public List<Specialday> getSpecialDaysInTheYear(){
+		return specialDayManager.getSpecialDaysInTheYear(null);
+	}
+	
+	@GetMapping(path = "/{year}")
+	public List<Specialday> getSpecialDaysInTheYear(@PathVariable("year") String yearRequest){
+		return specialDayManager.getSpecialDaysInTheYear(yearRequest);
+	}
+	
+	@PostMapping
+	public Specialday saveSpecialday(@RequestBody Specialday specialdayRequest) {
+		return specialDayManager.createSpecialday(specialdayRequest);
+	}
+	
+	@DeleteMapping()
+	public void deleteSpecialday(@RequestBody int id) {
+		specialDayManager.deleteSpecialday(id);
+	}
+
+}
