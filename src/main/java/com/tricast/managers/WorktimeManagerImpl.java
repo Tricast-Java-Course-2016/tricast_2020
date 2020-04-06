835af095b466d0e966974726602c76662077e46a
Juhász Kristóf <juhkris96@gmail.com>
2020-04-06 11:32:32 +0200

    WHT-42 WorktimeManagerImpl updated

--- src/main/java/com/tricast/managers/WorktimeManagerImpl.java
+++ src/main/java/com/tricast/managers/WorktimeManagerImpl.java
@@ -1,13 +1,14 @@
 package com.tricast.managers;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.tricast.api.requests.WorktimeCreationRequest;
+import com.tricast.api.responses.WorktimeCreationResponse;
 import com.tricast.repositories.WorktimeRepository;
 import com.tricast.repositories.entities.Worktime;
-import com.tricast.repositories.entities.enums.WorktimeType;
 
 @Component
 public class WorktimeManagerImpl implements WorktimeManager{
@@ -15,7 +16,7 @@ public class WorktimeManagerImpl implements WorktimeManager{
 	private WorktimeRepository worktimeRepository;
 
 	
-	
+	@Autowired
 	public WorktimeManagerImpl(WorktimeRepository worktimeRepository) {
 		this.worktimeRepository = worktimeRepository;
 	}
@@ -35,20 +36,50 @@ public class WorktimeManagerImpl implements WorktimeManager{
 		// TODO Auto-generated method stub
 		return worktimeRepository.save(worktimeRequest);
 	}
+	
+	@Override
+	public WorktimeCreationResponse createWorktimeFromRequest(WorktimeCreationRequest worktimeCreationRequest) {
+		Worktime newWorktime = mapWorktimeCreationRequestToWorktime(worktimeCreationRequest);
+		Worktime createdWorktime = worktimeRepository.save(newWorktime);
+		return mapWorktimeToWorktimeCreationResponse(createdWorktime);
+	}
 
 	@Override
 	public void deleteById(long id) {
 		worktimeRepository.deleteById(id);
 		
 	}
-/*
+	
+	/*
 	@Override
 	public List<Worktime> getAllByType(WorktimeType worktimeType) {
 		return worktimeRepository.findByType(worktimeType);
 	}*/
+	
+	private Worktime mapWorktimeCreationRequestToWorktime(WorktimeCreationRequest worktimeCreationRequest) {
+		Worktime newWorktime = new Worktime();
+		newWorktime.setComment(worktimeCreationRequest.getComment());
+		newWorktime.setEndTime(worktimeCreationRequest.getEndTime());
+		newWorktime.setStartTime(worktimeCreationRequest.getStartTime());
+		newWorktime.setModifiedBy(worktimeCreationRequest.getModifiedBy());
+		newWorktime.setType(worktimeCreationRequest.getType());
+		newWorktime.setWorkdayId(worktimeCreationRequest.getWorkdayId());
+		
+		return newWorktime;
+	}
 
-	
-	
+	private WorktimeCreationResponse mapWorktimeToWorktimeCreationResponse(Worktime worktime) {
+		WorktimeCreationResponse createdWorktime = new WorktimeCreationResponse();
+		createdWorktime.setId(worktime.getId());
+		createdWorktime.setComment(worktime.getComment());
+		createdWorktime.setEndTime(worktime.getEndTime());
+		createdWorktime.setStartTime(worktime.getStartTime());
+		createdWorktime.setModifiedBy(worktime.getModifiedBy());
+		createdWorktime.setType(worktime.getType());
+		createdWorktime.setWorkdayId(worktime.getWorkdayId());
+		
+		return createdWorktime;
+	}
 	
 	
 }
