52364aac6d6e4d016b3266d054b0369850f35ebd
Juhász Kristóf <juhkris96@gmail.com>
2020-04-06 11:28:23 +0200

    WHT-42 WorkdayManager Get-Creation-Del methods added

--- src/main/java/com/tricast/managers/WorkdayManager.java
+++ src/main/java/com/tricast/managers/WorkdayManager.java
@@ -3,6 +3,12 @@ package com.tricast.managers;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.tricast.api.requests.WorkdayCreationRequest;
+import com.tricast.api.responses.WorkdayCreationResponse;
+import com.tricast.api.responses.WorkdayGetResponse;
+import com.tricast.api.responses.WorkdayWorktimesGetResponse;
 import com.tricast.repositories.entities.Workday;
 
 public interface WorkdayManager {
@@ -11,6 +17,16 @@ public interface WorkdayManager {
 	Iterable<Workday> getAll();
 	
 	Workday createWorkday(Workday workdayRequest);
+	
+	Workday updateWorkday(Workday workdayRequest, long id);
+	
+	WorkdayCreationResponse updateUserWorkdayByDate(WorkdayCreationRequest workdayCreationRequest, String day);
+	
+	List<WorkdayGetResponse> getAllWorkdayByUserIdAndMonth();
+	
+	WorkdayWorktimesGetResponse getUserWorktimesByDate(String date);
+	
+	WorkdayCreationResponse createWorkdayFromRequest(WorkdayCreationRequest workdayCreationRequest);
 	
 	void deleteById(long id);
 }
