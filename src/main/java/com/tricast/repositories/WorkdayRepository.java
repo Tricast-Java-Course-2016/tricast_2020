60d2a611fcbdbacbc00fabb46deddd5a4e49e450
Albrecht Márk <alb.mark94@gmail.com>
2020-04-07 13:48:43 +0200

    Added new jpa query (findByUserIdAndDateBetween)

--- src/main/java/com/tricast/repositories/WorkdayRepository.java
+++ src/main/java/com/tricast/repositories/WorkdayRepository.java
@@ -1,9 +1,13 @@
 package com.tricast.repositories;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tricast.repositories.entities.Workday;
+import com.tricast.repositories.entities.Worktime;
 
 public interface WorkdayRepository extends CrudRepository<Workday, Long> {
-
+	List<Workday> findByUserIdAndDateBetween(int id,ZonedDateTime startDateTime , ZonedDateTime finishDate);
 }
