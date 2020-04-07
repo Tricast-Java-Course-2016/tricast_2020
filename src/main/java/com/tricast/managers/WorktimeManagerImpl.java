4881467d11b61b33e7cf9a4a39d8ef858f991af4
Albrecht Márk <alb.mark94@gmail.com>
2020-04-07 18:22:59 +0200

    Added created workday with work times method

--- src/main/java/com/tricast/managers/WorktimeManagerImpl.java
+++ src/main/java/com/tricast/managers/WorktimeManagerImpl.java
@@ -1,25 +1,31 @@
 package com.tricast.managers;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.tricast.api.requests.WorkdayCreationRequest;
 import com.tricast.api.requests.WorktimeCreationRequest;
+import com.tricast.api.responses.WorkdayCreationResponse;
 import com.tricast.api.responses.WorktimeCreationResponse;
+import com.tricast.repositories.WorkdayRepository;
 import com.tricast.repositories.WorktimeRepository;
+import com.tricast.repositories.entities.Workday;
 import com.tricast.repositories.entities.Worktime;
 
 @Component
 public class WorktimeManagerImpl implements WorktimeManager{
 	
 	private WorktimeRepository worktimeRepository;
-
+	private WorkdayRepository workdayRepository;
 	
 	@Autowired
-	public WorktimeManagerImpl(WorktimeRepository worktimeRepository) {
+	public WorktimeManagerImpl(WorktimeRepository worktimeRepository, WorkdayRepository workdayRepository) {
 		this.worktimeRepository = worktimeRepository;
+		this.workdayRepository = workdayRepository;
 	}
 
 	@Override
@@ -39,12 +45,80 @@ public class WorktimeManagerImpl implements WorktimeManager{
 	}
 	
 	@Override
-	public WorktimeCreationResponse createWorktimeFromRequest(WorktimeCreationRequest worktimeCreationRequest) {
-		Worktime newWorktime = mapWorktimeCreationRequestToWorktime(worktimeCreationRequest);
-		Worktime createdWorktime = worktimeRepository.save(newWorktime);
-		return mapWorktimeToWorktimeCreationResponse(createdWorktime);
+	public WorkdayCreationResponse createWorkdayWithWorktimeFromRequest(WorkdayCreationRequest workdayCreationRequest) {
+		
+		Workday newWorkday = RequestWorkdayMapper(workdayCreationRequest);
+		List<Worktime> newWorktime = mapWorktimeCreationRequestToWorktime(workdayCreationRequest);
+
+		Workday createdWorkday = workdayRepository.save(newWorkday);
+		List <Worktime> createdWorktimes = (List<Worktime>) worktimeRepository.saveAll(newWorktime);
+
+		WorkdayCreationResponse responseWorkday = ResponsenewWorkday(createdWorkday);
+		List <WorktimeCreationResponse> WorktimeCreationResponse = ResponsenewWorktimes(createdWorktimes);
+
+		return ResponsenNewWorkdayWithWorktimes(responseWorkday,WorktimeCreationResponse);
+		
+	}
+	
+	private Workday RequestWorkdayMapper(WorkdayCreationRequest workdayCreationRequest) {
+		Workday newWorkDay = new Workday();
+		newWorkDay.setDate(workdayCreationRequest.getDate());
+		newWorkDay.setUserId(workdayCreationRequest.getUserId());
+		return newWorkDay;
+	}
+	
+	private WorkdayCreationResponse ResponsenewWorkday(Workday createdWorkday) {
+		WorkdayCreationResponse workdayCreationResponse = new WorkdayCreationResponse();
+		workdayCreationResponse.setDate(createdWorkday.getDate());
+		workdayCreationResponse.setId(createdWorkday.getId());
+		workdayCreationResponse.setUserId(createdWorkday.getUserId());
+		return workdayCreationResponse;
+	}
+	
+	private List<WorktimeCreationResponse> ResponsenewWorktimes(List <Worktime> createdWorktimes) {
+		
+		List<WorktimeCreationResponse> responseWorktimes = new LinkedList<WorktimeCreationResponse>();
+		WorktimeCreationResponse worktimeCreationResponse = new WorktimeCreationResponse();
+		
+		for (Worktime newWorktimes : createdWorktimes) {
+			worktimeCreationResponse.setComment(newWorktimes.getComment());
+			worktimeCreationResponse.setEndTime(newWorktimes.getEndTime());
+			worktimeCreationResponse.setStartTime(newWorktimes.getStartTime());
+			worktimeCreationResponse.setModifiedBy(newWorktimes.getModifiedBy());
+			worktimeCreationResponse.setType(newWorktimes.getType());
+			worktimeCreationResponse.setWorkdayId(newWorktimes.getWorkdayId());
+			worktimeCreationResponse.setWorkdayId(newWorktimes.getWorkdayId());
+			responseWorktimes.add(worktimeCreationResponse);
+		}
+		return responseWorktimes;
 	}
 
+	private WorkdayCreationResponse ResponsenNewWorkdayWithWorktimes(WorkdayCreationResponse responseWorkday, List<WorktimeCreationResponse> createdWorktimes) {
+		 responseWorkday.setWorktimesCreatioenResponse(createdWorktimes);
+		 return responseWorkday;
+	}
+
+	private List<Worktime> mapWorktimeCreationRequestToWorktime(WorkdayCreationRequest worktimeCreationRequest) {
+		List<Worktime> newWorktimes = new LinkedList<Worktime>();
+		Worktime newWorktime = new Worktime();
+		List<WorktimeCreationRequest> worktimes = worktimeCreationRequest.getWorktimesCreationRequest();
+		for (WorktimeCreationRequest worktime : worktimes) {
+			newWorktime.setComment(worktime.getComment());
+			newWorktime.setEndTime(worktime.getEndTime());
+			newWorktime.setStartTime(worktime.getStartTime());
+			newWorktime.setModifiedBy(worktime.getModifiedBy());
+			newWorktime.setType(worktime.getType());
+			newWorktime.setWorkdayId(worktime.getWorkdayId());
+			newWorktime.setWorkdayId(worktime.getWorkdayId());
+			newWorktimes.add(newWorktime);
+		}
+		return newWorktimes;
+	}
+	
+	
+	
+	
+	
 	@Override
 	public void deleteById(long id) {
 		worktimeRepository.deleteById(id);
@@ -63,18 +137,6 @@ public class WorktimeManagerImpl implements WorktimeManager{
 	public List<Worktime> getAllByType(WorktimeType worktimeType) {
 		return worktimeRepository.findByType(worktimeType);
 	}*/
-	
-	private Worktime mapWorktimeCreationRequestToWorktime(WorktimeCreationRequest worktimeCreationRequest) {
-		Worktime newWorktime = new Worktime();
-		newWorktime.setComment(worktimeCreationRequest.getComment());
-		newWorktime.setEndTime(worktimeCreationRequest.getEndTime());
-		newWorktime.setStartTime(worktimeCreationRequest.getStartTime());
-		newWorktime.setModifiedBy(worktimeCreationRequest.getModifiedBy());
-		newWorktime.setType(worktimeCreationRequest.getType());
-		newWorktime.setWorkdayId(worktimeCreationRequest.getWorkdayId());
-		
-		return newWorktime;
-	}
 
 	private WorktimeCreationResponse mapWorktimeToWorktimeCreationResponse(Worktime worktime) {
 		WorktimeCreationResponse createdWorktime = new WorktimeCreationResponse();
