cbefb4b82dbdb147015846ef907c4f09209e907a
Albrecht Márk <alb.mark94@gmail.com>
2020-04-08 08:54:30 +0200

    Deleted useless rows and  getAllWorktimeByWorktimeId, deleteAllWorkTimesById mothods

--- src/main/java/com/tricast/managers/WorktimeManager.java
+++ src/main/java/com/tricast/managers/WorktimeManager.java
@@ -1,25 +1,21 @@
 package com.tricast.managers;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tricast.api.requests.WorkTimeUpdateListRequest;
 import com.tricast.api.requests.WorkdayCreationRequest;
-import com.tricast.api.requests.WorktimeCreationRequest;
 import com.tricast.api.responses.WorkdayCreationResponse;
-import com.tricast.api.responses.WorktimeCreationResponse;
 import com.tricast.repositories.entities.Worktime;
-import com.tricast.repositories.entities.enums.WorktimeType;
 
 public interface WorktimeManager {
 	
-	Iterable<Worktime> getAll();
+	List<Worktime> getAllWorktimeByWorktimeId(long id);
 	
 	List<Worktime> saveModified(WorkTimeUpdateListRequest worktimesListRequest, long workDayid);
 	
+	//List<Worktime> getAllByType(WorktimeType worktype);
 	
 	WorkdayCreationResponse createWorkdayWithWorktimeFromRequest( WorkdayCreationRequest workdayCreationRequest);
 
+	void deleteAllWorkTimesById(long id);
 }
