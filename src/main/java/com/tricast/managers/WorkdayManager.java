3c01d0aceb1ee1d5311a2aa12830262645bbb6a5
Albrecht Márk <alb.mark94@gmail.com>
2020-04-13 15:34:51 +0200

    Changed method1s return

--- src/main/java/com/tricast/managers/WorkdayManager.java
+++ src/main/java/com/tricast/managers/WorkdayManager.java
@@ -1,16 +1,8 @@
 package com.tricast.managers;
 
-import java.util.List;
-import java.util.Optional;
+import com.tricast.api.responses.WorkdayWithWorkHoursStatsGetResponse;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.tricast.api.requests.WorkdayCreationRequest;
-import com.tricast.api.responses.WorkdayCreationResponse;
-import com.tricast.api.responses.WorkdayGetResponse;
-import com.tricast.repositories.entities.Workday;
-
-public interface WorkdayManager {	
-	List<WorkdayGetResponse> getAllWorkdayByUserIdAndMonth(int id);
+public interface WorkdayManager {
+	WorkdayWithWorkHoursStatsGetResponse getAllWorkdayByUserIdAndMonth(int id);
 	void deleteById(long id);
 }
