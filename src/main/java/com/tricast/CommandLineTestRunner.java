1d3c796961185e59f5d208adc124cd9d0c379cbc
Albrecht Márk <alb.mark94@gmail.com>
2020-04-14 11:00:35 +0200

    Removed Specialdays and workdays and worktimes test rows

--- src/main/java/com/tricast/CommandLineTestRunner.java
+++ src/main/java/com/tricast/CommandLineTestRunner.java
@@ -1,14 +1,7 @@
 package com.tricast;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 import javax.transaction.Transactional;
 
-import org.hibernate.internal.util.ZonedDateTimeComparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.tricast.repositories.OffDayRepository;
 import com.tricast.repositories.OffDayLimitsRepository;
-import com.tricast.repositories.SpecialdayRepository;
+import com.tricast.repositories.OffDayRepository;
 import com.tricast.repositories.UserRepository;
-import com.tricast.repositories.WorkdayRepository;
-import com.tricast.repositories.WorktimeRepository;
-import com.tricast.repositories.entities.enums.WorktimeType;
 
 @SpringBootApplication
 public class CommandLineTestRunner implements CommandLineRunner {
@@ -47,5 +36,8 @@ public class CommandLineTestRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         LOG.info("Running...");
         // add your code here
+        userRepository.findAll().forEach(user -> System.out.println(user.getGender()));
+        offdayLimitsRepository.findAll().forEach(offdaylimits -> System.out.println(offdaylimits.getMaximumAmount()));
+        offdayrepository.findAll().forEach(offday -> System.out.println(offday.getStatus()));
     }
 }
