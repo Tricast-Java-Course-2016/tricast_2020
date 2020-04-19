c4ebd3e2254c7955ad063f20a946a33270f0fa7d
Juhász Kristóf <juhkris96@gmail.com>
2020-04-19 13:08:47 +0200

    With Example

--- src/main/webapp/js/worktimesUtil.js
+++ src/main/webapp/js/worktimesUtil.js
@@ -1,7 +1,19 @@
 window.SB = window.SB || {};
 
 window.SB.Utils = {
-    
+		//EXAMPLE
+	    readFormData : function readFormData($form) {
+	        // The FormData interface provides a way to easily construct a set of key/value pairs representing form fields
+	        // and their values, which can then be easily sent in a http request
+	        // The FormData.entries() method returns an iterator allowing to go through all key/value pairs contained in
+	        // this object.
+	        let data = {};
+	        for(let val of (new FormData($form.get(0))).entries()) {
+	            data[val[0]] = val[1];
+	        }
+	        return data;
+	    },
+	//My DATAREADER
     readWorktimesFormDataList : function readFormDataList($form){
     	let dataList = [];
     	let data = {};
