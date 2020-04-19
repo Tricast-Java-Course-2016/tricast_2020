776484d2b4dd25ca7ccc7c20ceac79d820bf5fb0
Vasas Zsuzsanna <susan.katona@gmail.com>
2020-04-19 15:51:56 +0200

    WHT-46 manage accounts - search, save, update

--- src/main/webapp/js/manageaccounts.js
+++ src/main/webapp/js/manageaccounts.js
@@ -18,18 +18,11 @@ function searchUserSubmit() {
 
 	let url = "/workinghours/rest/users/search?userName=" + user;
 
-	console.log(url);
-	// alert(url);
-
 	$.getJSON(url).done(function(data) {
-		// Successful call
-		console.log("jott adat a servetol");
 		displaySearchedUser(data);
 	}).fail(function(jqXHR, textStatus, errorThrown) {
-		// Error happened
 		SB.Utils.defaultErrorHandling(jqXHR);
 	}).always(function() {
-		// Run always
 		console.log("complete");
 	});
 }
@@ -37,14 +30,10 @@ function searchUserSubmit() {
 function displaySearchedUser(data) {
 	let searchedUser = [];
 
-	console.log(data.id);
-
 	$('#manAccFirstName').val(data.firstName);
 	$('#manAccMiddleName').val(data.middleName);
 	$('#manAccLastName').val(data.lastName);
 	$('#manAccUserName').val(data.userName);
-
-	// $('#manAccPassword').val(data.id);
 	$('#manAccEmail').val(data.email);
 	$('#manAccGenderField').val(data.gender);
 	$('#manAccDob').val(data.dob);
@@ -53,26 +42,18 @@ function displaySearchedUser(data) {
 	$('#manAccAddress').val(data.address);
 	$('#manAccCompanyName').val(data.companyName);
 	$('#manAccRoleNameField').val(data.roleId);
-
+	$('#id').val(data.id);
 	if (data.active)
 		$('#manAccActiveField').val('1');
 	else
 		$('#manAccActiveField').val('0');
-
-	$('#id').val(data.id);
-
-	// user name input field disabled
-	// $('#manAccUserName').prop("disabled", true);
 }
 
 function saveChangesSubmit() {
 	let data = SB.Utils.readFormData($('#postUser'));
-	console.log("most jön a Hidden id");
-	console.log($('#id').val());
 	if ($('#id').val()) {
 		$.ajax({
 			type : 'PUT',
-
 			dataType : 'json',
 			url : "/workinghours/rest/users/update",
 			headers : {
@@ -80,18 +61,15 @@ function saveChangesSubmit() {
 			},
 			data : JSON.stringify(data)
 		});
+		alert('OK Updated');
 
 	} else {
-
 		$.post("/workinghours/rest/users/create", JSON.stringify(data),
 				function(data) {
-					//
-					alert('OK Save');
+					alert('OK Saved');
 				});
-
 	}
-
-	// field cleanup
+	
 	$('#manAccFirstName').val("");
 	$('#manAccMiddleName').val("");
 	$('#manAccLastName').val("");
@@ -108,5 +86,4 @@ function saveChangesSubmit() {
 	$('#manAccRoleNameField').val("");
 	$('#manAccActiveField').val("");
 	$('#searchedUser').val("");
-
 }
