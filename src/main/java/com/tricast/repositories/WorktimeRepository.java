b7ae21737440b56b584af95b3657b029341edf2e
Albrecht Márk <alb.mark94@gmail.com>
2020-04-20 15:11:04 +0200

    Delete row

--- src/main/java/com/tricast/repositories/WorktimeRepository.java
+++ src/main/java/com/tricast/repositories/WorktimeRepository.java
@@ -11,5 +11,4 @@ public interface WorktimeRepository extends CrudRepository<Worktime, Long> {
 	List<Worktime> findByType(WorktimeType worktimeType);
 	List<Worktime> findAllByWorkdayId(long id);
 	List<Worktime> findAllByWorkdayIdIn(List<Long> id);
-    List<Worktime> saveAll(List<Worktime> worktimes);
 }
