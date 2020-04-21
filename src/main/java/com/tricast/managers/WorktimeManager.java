c804615c9f078ccf4c95189bcf1f00902068b3bc
Albrecht Márk <alb.mark94@gmail.com>
2020-04-21 12:31:14 +0200

    Changed return types

--- src/main/java/com/tricast/managers/WorktimeManager.java
+++ src/main/java/com/tricast/managers/WorktimeManager.java
@@ -7,18 +7,19 @@ import com.tricast.api.requests.WorkdayCreationRequest;
 import com.tricast.api.responses.WorkdayCreationResponse;
 import com.tricast.repositories.entities.Worktime;
 import com.tricast.api.responses.WorkTimeStatByIdResponse;
+import com.tricast.api.responses.WorktimesUpdateResponse;
 
 public interface WorktimeManager {
 	
 	List<Worktime> getAllWorktimeByWorktimeId(long workdayId);
 	
-	List<Worktime> saveModified(WorkTimeUpdateListRequest worktimesListRequest, long workDayid);
+	WorktimesUpdateResponse saveModified(WorkTimeUpdateListRequest worktimesListRequest, long workDayid);
 	
 	//List<Worktime> getAllByType(WorktimeType worktype);
 	
 	WorkdayCreationResponse createWorkdayWithWorktimeFromRequest( WorkdayCreationRequest workdayCreationRequest);
 
-	void deleteAllWorkTimesById(long id);
+	int deleteAllWorkTimesById(long id);
 
 	WorkTimeStatByIdResponse workTimeStatByIdResponse(long id,int year);
 }
