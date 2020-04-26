9e05f87d5f72edfafcc3cea890f53639d07d4f95
Albrecht Márk <alb.mark94@gmail.com>
2020-04-26 12:46:08 +0200

    WHT-57 added role id to method

--- src/main/java/com/tricast/managers/WorkdayManager.java
+++ src/main/java/com/tricast/managers/WorkdayManager.java
@@ -3,6 +3,6 @@ package com.tricast.managers;
 import com.tricast.api.responses.WorkdayWithWorkHoursStatsGetResponse;
 
 public interface WorkdayManager {
-	WorkdayWithWorkHoursStatsGetResponse getAllWorkdayByUserIdAndMonth(int id);
+	WorkdayWithWorkHoursStatsGetResponse getAllWorkdayByUserIdAndMonth(int id,int roleId);
 	void deleteById(long id);
 }
