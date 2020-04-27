396a85657fc4504eba5b55b55841c65658a0835b
Albrecht Márk <alb.mark94@gmail.com>
2020-04-27 19:14:47 +0200

    removed useless rows and modified the methods, Added respons entity, and added user check

--- src/main/java/com/tricast/controllers/WorkdayController.java
+++ src/main/java/com/tricast/controllers/WorkdayController.java
@@ -1,27 +1,13 @@
 package com.tricast.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tricast.api.requests.WorkdayCreationRequest;
-import com.tricast.api.responses.WorkdayCreationResponse;
-import com.tricast.api.responses.WorkdayGetResponse;
-import com.tricast.api.responses.WorkdayStatsResponse;
-import com.tricast.api.responses.WorkdayWithWorkHoursStatsGetResponse;
 import com.tricast.managers.WorkdayManager;
-import com.tricast.managers.WorktimeManager;
-import com.tricast.repositories.entities.Workday;
-import com.tricast.repositories.entities.Worktime;
 import com.tricast.repositories.entities.enums.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,28 +16,36 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 @RestController
 @RequestMapping(path = "rest/workdays")
 public class WorkdayController {
-	
-	@Autowired
-	private WorkdayManager workdayManager;	
-	
-	@GetMapping(path = "/workedhours/{userId}")
-	public ResponseEntity<?> getAllWorkdaysByIdAndMonth(@RequestAttribute("authentication.roleId")int roleId ,@PathVariable("userId") int userId){
-        try {
-            return ResponseEntity.ok(workdayManager.getAllWorkdayByUserIdAndMonth(userId,roleId));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-	}
 
-	
-	@DeleteMapping(path = "/{wordayId}")
-	public ResponseEntity<?> deleteWorkday(@PathVariable("wordayId") long wordayId){
+    @Autowired
+    private WorkdayManager workdayManager;
+
+    @GetMapping(path = "/workedhours/{userId}")
+    public ResponseEntity<?> getAllWorkdaysByIdAndMonth(@RequestAttribute("authentication.roleId") int roleId, @RequestAttribute("authentication.userId") int loggedUserId, @PathVariable("userId") int userId) {
+        if (userCheck(roleId, loggedUserId, userId)) {
+            try {
+                return ResponseEntity.ok(workdayManager.getAllWorkdayByUserIdAndMonth(userId, roleId));
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Permission denied");
+        }
+    }
+
+    @DeleteMapping(path = "/{wordayId}")
+    public ResponseEntity<?> deleteWorkday(@PathVariable("wordayId") long wordayId) {
         try {
             workdayManager.deleteById(wordayId);
             return ResponseEntity.ok("DELETE SUCCESSFUL");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-	}
-	
+    }
+
+    private boolean userCheck(int roleId, int loggedInUser, int inRequestUserID) {
+        return Role.ADMIN == Role.getById(roleId) || loggedInUser == inRequestUserID;
+    }
+
 }
